4dc0c75475e5bf3b759396f97444d29caf3ef4e0
Shawn0604 <shawn901011@gmail.com>
2023-10-16 20:14:40 +0800

    emotion

--- src/api.js
+++ src/api.js
@@ -1,235 +1,40 @@
-const express = require('express')
-const Session = require('./model')
-const OpenAIChat = require('./utils/openai')
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const Session = require('./models/session');
+const openAIChat = require('./utils/openai');
+const cors = require('cors');
 
-const router = express.Router()
+const app = express();
+app.use(cors());
 
-router.get("/test", (req, res) => {
-  res.send({ msg: "you are amazing!" });
-})
+const corsOptions = {
+    origin: '*', // 指定前端的地址
+    methods: 'GET,POST',
+    credentials: true, // 启用发送认证信息
+};
 
-// Create session.
-router.post('/users/:userId/sessions', (req, res) => {
-	// 從 request 中獲取 'userId' 參數
-	const userId = req.params.userId;
+router.use(cors(corsOptions));
 
-	// 檢查 'postId' 參數是否存在
-	if (!userId) {
-		res.status(400).send({
-			message: 'User ID is required',
-		});
-		return;
-	}
+router.post("/process-diary", async (req, res) => {
+    const { text } = req.body;
 
-  try {
-    const newSession = new Session({
-      user: userId,
-      title: ""
-    });
-    console.log(newSession);
-
-    newSession.save().then(data => {
-      const savedDocument = {
-        _id: data._id,
-        user: data.user,
-        title: data.title,
-        createdAt: data.createdAt,
-        messages: data.messages
-      };
-      res.send(savedDocument);
-    })
-  } catch (error){
-    console.error(error);
-    res.status(500).send({
-      message: 'Internal Server Error'
-    });
-  }
-  
-})
-
-// Use userId to search for all corresponding sessions.
-router.get('/users/:userId/sessions', (req, res) => {
-  const userId = req.params.userId;
-
-	if (!userId) {
-		res.status(400).send({
-			message: 'User ID is required',
-		});
-		return;
-	}
-
-  (async () => {
     try {
-      const ses = await Session.find({ user: userId }).select('_id user title createdAt');
-      res.send(ses);
-    } catch (error){
-      console.error(error);
-      res.status(500).send({
-        message: 'Internal Server Error'
-      });
+        const openAIResponse = await openAIChat([text]);
+
+        const newSession = new Session({
+            createdAt: Date.now(),
+            title: "",
+            messages: text,
+            openAIResponse: openAIResponse
+        });
+
+        const savedSession = await newSession.save();
+
+        res.json({ message:  openAIResponse });
+    } catch (error) {
+        res.json({ message: JSON.stringify(openAIResponse) });
     }
-  })();
-})
-
-// Use sessionId to search for a Session object.
-router.get('/session/:sessionId', (req, res) => {
-	// 從 request 中獲取 'sessionId' 參數
-	const sessionId = req.params.sessionId;
-
-	// 檢查 'sessionId' 參數是否存在
-	if (!sessionId) {
-		res.status(400).send({
-			message: 'Session ID is required',
-		});
-		return;
-	}
-
-  (async () => {
-    try {
-      const ses = await Session.findById(sessionId).select('_id user title messages createdAt');
-      if (!ses) {
-        res.status(404).send({
-          message: 'Session not found',
-        });
-        return;
-      }
-      res.send(ses)
-    } catch (error){
-      console.error(error);
-      if (error.name === 'CastError' || error.name === 'DocumentNotFoundError') {
-        res.status(404).send({
-          message: 'Session not found'
-        });
-      } else {
-        res.status(500).send({
-          message: 'Internal Server Error'
-        });
-      }
-    }
-  })();
-})
-
-// Use sessionId to delete a Session object.
-router.delete('/session/:sessionId/', (req, res) => {
-  const sessionId = req.params.sessionId;
-
-  if (!sessionId) {
-    res.status(400).send({
-      message: 'Session ID is required',
-    });
-    return;
-  }
-
-  (async () => {
-    try {
-      const ses = await Session.findByIdAndDelete(sessionId);
-      if (!ses) {
-        res.status(404).send({
-          message: 'Session not found',
-        });
-        return;
-      }
-      res.send({
-        message: 'Session deleted'
-      });
-    } catch (error){
-      console.error(error);
-      if (error.name === 'CastError' || error.name === 'DocumentNotFoundError') {
-        res.status(404).send({
-          message: 'Session not found'
-        });
-      } else {
-        res.status(500).send({
-          message: 'Internal Server Error'
-        });
-      }
-    }
-  })();
-
-})
-
-// Update the title of a Session.
-router.put('/session/:sessionId', (req, res) => {
-  const sessionId = req.params.sessionId;
-  const title = req.query.title
-
-  if (!sessionId || !title) {
-    res.status(400).send({
-      message: 'Session ID and title is required',
-    });
-    return;
-  }
-
-  console.log(title);
-
-  (async () => {
-    try {
-      const ses = await Session.findByIdAndUpdate(sessionId, { $set: {title} }, {new: true}).select('_id user title messages createdAt');
-      if (!ses) {
-        res.status(404).send({
-          message: 'Session not found',
-        });
-        return;
-      }
-      res.send(ses);
-    } catch (error){
-      console.error(error);
-      if (error.name === 'CastError' || error.name === 'DocumentNotFoundError') {
-        res.status(404).send({
-          message: 'Session not found'
-        });
-      } else {
-        res.status(500).send({
-          message: 'Internal Server Error'
-        });
-      }
-    }
-  })();
-
-})
-
-// Update the messages of a Session by OpenAI API.
-router.put('/session/:sessionId/messages', (req, res) => {
-  const sessionId = req.params.sessionId;
-  const content = req.body.content;
-
-  if (!sessionId || !content) {
-    res.status(400).send({
-      message: 'Content and sessionId is required',
-    });
-    return;
-  }
-
-  console.log(content);
-
-  (async () => {
-    try {
-      var ses = await Session.findByIdAndUpdate(sessionId, { $push: {messages: content} }, {new: true});
-      if (!ses) {
-        res.status(404).send({
-          message: 'Session not found',
-        });
-        return;
-      }
-
-      OpenAIChat(ses.messages).then((value) => {
-        return Session.findByIdAndUpdate(sessionId, { $push: {messages: value.message.content} }, {new: true}).select('_id user title messages createdAt');
-      }).then((session) => res.send(session));
-
-    } catch (error){
-      console.error(error);
-      if (error.name === 'CastError' || error.name === 'DocumentNotFoundError') {
-        res.status(404).send({
-          message: 'Session not found'
-        });
-      } else {
-        res.status(500).send({
-          message: 'Internal Server Error'
-        });
-      }
-    }
-  })();
-
-})
+});
 
 module.exports = router;
--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,27 +2,33 @@ const OpenAI = require('openai')
 
 const transformMessages = require('./helper')
 
-require('dotenv').config()
-
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
 // 傳入 messages，取得 OpenAI API 的回覆
 const openAIChat = async (messages) => {
     // 輸入空訊息直接回傳
-	if (messages.length == 0) {
-		return
+	if (messages.length === 0) {
+		return "沒有收到訊息";
 	} else {
         // 轉換成 openai 格式
-		messages = transformMessages(messages)
-        console.log(messages)
-        // 呼叫 openai api
-		const completion = await openai.chat.completions.create({
-			messages,
-			model: 'gpt-3.5-turbo',
-			max_tokens: 80,
-            temperature: 0.9,
-		})
-		return completion.choices[0]
+
+		messages = transformMessages(messages); // 转换消息格式
+
+		// 在messages前添加系统消息
+		const messagesWithSystemMsg = [
+    		{ role: 'system', content: '使用繁體中文回應，為我的回應進行情緒評分，評分範圍為1~5，以及提供我這段文字的建議' },
+    		...messages,
+		];
+
+// 调用 OpenAI API
+			const completion = await openai.chat.completions.create({
+				messages: messagesWithSystemMsg, // 使用包含系统消息的数组
+				model: 'gpt-3.5-turbo',
+				max_tokens: 200,
+				temperature: 0.9,
+			});
+
+			return completion.choices[0].message.content;
 	}
 }
 
